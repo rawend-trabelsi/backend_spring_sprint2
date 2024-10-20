7f949df1186b3a81fced590515298c6a2a23be20
rawend <rawendtrabelsi034@gmail.com>
2024-10-20 13:12:37 +0200

    backend spring sprint2

--- src/main/java/com/rawend/smartphones/serevice/SmartphoneServiceImpl.java
+++ src/main/java/com/rawend/smartphones/serevice/SmartphoneServiceImpl.java
@@ -3,6 +3,7 @@ package com.rawend.smartphones.serevice;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import com.rawend.smartphones.entities.Smartphone;
@@ -14,6 +15,7 @@ import com.rawend.smartphones.entities.Type; // Assurez-vous que c'est bien celu
 public class SmartphoneServiceImpl  implements SmartphoneService{
 	@Autowired
 	SmartphoneRepository smartphoneRepository;
+	@PreAuthorize("hasAuthority('ADMIN')")
 	@Override
 	public Smartphone saveSmartphone(Smartphone s) {
 		
